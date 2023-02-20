25892b6144b95ada4ddbc300d4a56b2fc4689085
templar-command0 <templar.command0@gmail.com>
2023-02-21 00:05:01 +0530

    lightspeed rerenders using cookies caches brahmamuhurtha time in cookies. reduces request load on sunset-sunrise api server saves a lot of time in rerenders (provided location doesn't change)

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ app link - https://templar-command0.github.io/brahmamuhurta/
 
 ### What it means, what can happen? Here's what Sadhguru says about BrahmaMuhurtha
 
-![failed to load image](images/1.jpeg)
-![failed to load image](images/2.jpeg)
-![failed to load image](images/3.jpeg)
-![failed to load image](images/4.jpeg)
-![failed to load image](images/5.jpeg)
-![failed to load image](images/6.jpeg)
-![failed to load image](images/7.jpeg)
-![failed to load image](images/8.jpeg)
+![Brahmahurta intro introduction](images/1.jpeg)
+![brahmahurta time-period](images/2.jpeg)
+![what happens during brahmahurta](images/3.jpeg)
+![brahmahurta- the best time for spiritual progress](images/4.jpeg)
+![how to wake up at brahmahurta naturally](images/5.jpeg)
+![yogic practices](images/6.jpeg)
+![early dinner](images/7.jpeg)
+![early to bed](images/8.jpeg)
--- script.js
+++ script.js
@@ -1,52 +1,67 @@
 const timeDisplayEl = document.querySelector("#muhurat-display");
+const displayTime = (time) => (timeDisplayEl.innerHTML = time);
 const error = document.querySelector("#error");
 if (navigator.geolocation) {
   // device can return its location
   navigator.geolocation.getCurrentPosition(
     (position) => {
       error.style.display = "none";
-      const url = `https://api.sunrise-sunset.org/json?lat=${position.coords.latitude}&lng=${position.coords.longitude}&formatted=0`;
+      const positionString =
+        position.coords.latitude + "," + position.coords.longitude;
+      const cache = checkCache(positionString);
+      if (cache) {
+        displayTime(cache.split("=")[1]);
+      } else {
+        const url = `https://api.sunrise-sunset.org/json?lat=${position.coords.latitude}&lng=${position.coords.longitude}&formatted=0`;
+        fetch(url)
+          .then((res) => res.json())
+          .then((data) => data.results.sunrise)
+          .then((sunriseInUTC) => {
+            const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+            return new Date(sunriseInUTC).toLocaleString("en-US", {
+              timeZone: timeZone,
+            });
+          })
+          .then((sunriseInLocalTimeZone) => {
+            const date = new Date(sunriseInLocalTimeZone);
+            return date < new Date() ? date.setDate(date.getDate() + 1) : date;
+          })
+          .then((nextSunRise) => {
+            return {
+              begins: function () {
+                const date = new Date(nextSunRise);
+                date.setMinutes(date.getMinutes() - 96);
+                return date;
+              },
+              ends: function () {
+                const date = new Date(nextSunRise);
+                date.setMinutes(date.getMinutes() - 48);
+                return date;
+              },
+              readable: function (time) {
+                return time.toDateString();
+              },
+            };
+          })
+          .then((BrahmaMuhurtha) => {
+            function readableFormat(time) {
+              return time ? time.split(":").splice(0, 2).join(":") : false;
+            }
+            const result = `${readableFormat(
+              BrahmaMuhurtha.begins().toLocaleTimeString()
+            )} AM to ${readableFormat(
+              BrahmaMuhurtha.ends().toLocaleTimeString()
+            )} AM`;
 
-      fetch(url)
-        .then((res) => res.json())
-        .then((data) => data.results.sunrise)
-        .then((sunriseInUTC) => {
-          const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-          return new Date(sunriseInUTC).toLocaleString("en-US", {
-            timeZone: timeZone,
+            console.log("end time", BrahmaMuhurtha.ends());
+            displayTime(result);
+            setCache(
+              positionString,
+              result,
+              new Date(BrahmaMuhurtha.ends()).toUTCString()
+            );
           });
-        })
-        .then((sunriseInLocalTimeZone) => {
-          const date = new Date(sunriseInLocalTimeZone);
-          return date < new Date() ? date.setDate(date.getDate() + 1) : date;
-        })
-        .then((nextSunRise) => {
-          return {
-            begins: function () {
-              const date = new Date(nextSunRise);
-              date.setMinutes(date.getMinutes() - 96);
-              return date;
-            },
-            ends: function () {
-              const date = new Date(nextSunRise);
-              date.setMinutes(date.getMinutes() - 48);
-              return date;
-            },
-            readable: function (time) {
-              return time.toDateString();
-            },
-          };
-        })
-        .then((BrahmaMuhurtha) => {
-          function readableFormat(time) {
-            return time ? time.split(":").splice(0, 2).join(":") : false;
-          }
-          timeDisplayEl.innerHTML = `${readableFormat(
-            BrahmaMuhurtha.begins().toLocaleTimeString()
-          )} AM to ${readableFormat(
-            BrahmaMuhurtha.ends().toLocaleTimeString()
-          )} AM`;
-        });
+      }
     },
     () => {
       alert(
@@ -56,4 +71,16 @@ if (navigator.geolocation) {
       error.style.display = "block";
     }
   );
+}
+
+function setCache(locationInput, timingOutput, endsAt) {
+  document.cookie = `${locationInput}=${timingOutput};expires=${endsAt}`;
+}
+function checkCache(key) {
+  const x = document.cookie.split(";").filter((item) => {
+    return item.includes(key + "=");
+  });
+  if (x.length > 0) {
+    return x[0];
+  } else return false;
 }
